d7419c945382de1b710d1ac247e3d3f708f104f6
tuhv <tuhvph20024@fpt.edu.vn>
2024-09-30 19:59:30 +0700

    admin

---
+++ Web Bán Hàng.sql
@@ -0,0 +1,70 @@
+CREATE TABLE `Role` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(20)
+);
+
+CREATE TABLE `User` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `fullname` varchar(50),
+  `email` varchar(150),
+  `phoner_number` varchar(20),
+  `address` varchar(255),
+  `password` int(255),
+  `role_id` int,
+  `created_at` datetime,
+  `updated_at` datetime
+);
+
+CREATE TABLE `Category` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(100)
+);
+
+CREATE TABLE `Product` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `category_id` int,
+  `title` varchar(350),
+  `price` int,
+  `discount` int,
+  `thumbnail` varchar(500),
+  `description` longtext,
+  `created_at` datetime,
+  `updated_at` datetime
+);
+
+CREATE TABLE `Galery` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `product_id` int,
+  `thumbnail` varchar(500)
+);
+
+CREATE TABLE `Orders` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `fullname` varchar(50),
+  `email` varchar(150),
+  `phone_number` varchar(20),
+  `address` varchar(255),
+  `notes` varchar(500),
+  `order_date` datetime,
+  `status` int,
+  `total_money` int
+);
+
+CREATE TABLE `Order_Details` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `order_id` int,
+  `product_id` int,
+  `price` int,
+  `num` int,
+  `total_money` int
+);
+
+ALTER TABLE `User` ADD FOREIGN KEY (`role_id`) REFERENCES `Role` (`id`);
+
+ALTER TABLE `Product` ADD FOREIGN KEY (`category_id`) REFERENCES `Category` (`id`);
+
+ALTER TABLE `Order_Details` ADD FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`);
+
+ALTER TABLE `Galery` ADD FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`);
+
+ALTER TABLE `Order_Details` ADD FOREIGN KEY (`id`) REFERENCES `Orders` (`id`);
